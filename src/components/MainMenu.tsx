65fee5f85e06ba63118f8e12d702d3dcb47c2f0b
Syafiq1331 <bussinesyafiq@gmail.com>
2023-03-19 13:00:45 +0700

    "add main menu component"

---
+++ src/components/MainMenu.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './Header'
+import Content from './Content'
+
+const Main = () => {
+  return (
+    <main>
+      <Content />
+    </main>
+  )
+}
+
+export default Main
