333e9749a29b8a8dfd42aeecb9ede1f17ae9c78e
Syafiq1331 <bussinesyafiq@gmail.com>
2023-03-20 23:54:41 +0700

    "add new page component"

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import Header from '../components/Header';
+import Main from '../components/MainMenu';
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  )
+}
+
+export default Home
