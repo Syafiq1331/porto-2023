826950a21c3a4bffe10e8b311e3e01973581f872
Syafiq1331 <bussinesyafiq@gmail.com>
2023-03-21 08:36:19 +0700

    "update in work component"

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useEffect } from "react";
 import Card from './Card'
 import idn from '../../public/projects/idn_landingPage.png'
 import tekaje from '../../public/projects/tekaje_landingPage.png'
@@ -6,20 +7,22 @@ import bookshelf from '../../public/projects/bookshelf__App.png'
 import pemudacoding from '../../public/projects/pemudacoding__company.png';
 
 const Work = () => {
+
   return (
     <div className='bg-[#EDF7FA]'>
-      <div className='container mx-auto py-4'>
+      <div className='container mx-auto py-4 '>
         <div className='flex justify-between px-6 py-4'>
           <p className='font-semibold'>Recent projects</p>
           <a href=" " className='text-sky-500'>View All</a>
         </div>
-        <div className='grid grid-cols-1 md:grid-cols-2 gap-4 mx-4 my-4'>
+        <div className='grid grid-cols-1 md:grid-cols-2 gap-x-6 gap-y-8 mx-4 my-4 pb-6 lg:px-24 md:px-10 px-2'>
           <Card
             image={idn}
             link='https://polytechinc-idn.vercel.app/'
             title='Polytechinc IDN landing page'
             desc='Landing page about polytechinc IDN Bogor, Indonesia.'
             tech_1='Vercel' tech_2='Tailwindcss' tech_3='React Js'
+            data-aos="fade-left"
           />
           <Card
             image={tekaje}
