2ff4a0c9e0e98ee8c5ff2145be9da50a7ac3a4e4
Syafiq1331 <bussinesyafiq@gmail.com>
2023-03-21 08:36:28 +0700

    "update in contact component"

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -2,7 +2,29 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className=''>
+      <h1 className='text-center text-2xl font-semibold mt-12'>Reach Me ✨</h1>
+      <div className='flex justify-center mt-4'>
+        <div>
+          <span className='flex items-center gap-4'>
+            <i className='bx bxs-envelope text-red-500 text-4xl' ></i>
+            <a href='mailto:syafiqbussines@gmail.com' className='text-slate-800/70 underline text-md md:text-lg'>syafiqbussines@gmail.com</a>
+          </span>
+          <span className='flex items-center gap-4'>
+            <i className='bx bxl-telegram text-blue-500 text-4xl' ></i>
+            <a href='http://t.me/syafiq1331' className='text-slate-800/70 underline text-md md:text-lg'>Syafiqrzf</a>
+          </span>
+          <span className='flex items-center gap-4'>
+            <i className='bx bxl-linkedin-square text-blue-800 text-4xl' ></i>
+            <a href='https://www.linkedin.com/in/syafiqrzf1331/' target='_blank' className='text-slate-800/70 underline text-md md:text-lg'>Rizky Fauzi</a>
+          </span>
+          <span className='flex items-center gap-4'>
+            <i className='bx bxl-instagram-alt text-[#E1306C] text-4xl' ></i>
+            <a href='https://instagram.com/syafiqrzf' target='_blank' className='text-slate-800/70 underline text-md md:text-lg'>Syafiqrzf</a>
+          </span>
+        </div>
+      </div>
+    </div>
   )
 }
 
