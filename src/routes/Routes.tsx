f7cd5423116657340af8fbb7212e631a1a982672
Syafiq Rizky Fauzi <bussinesyafiq@gmail.com>
2023-04-18 08:21:39 +0700

    fix:add not found route

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import Portolio from '../pages/Portolio';
 import Contact from '../pages/Contact';
+import Page from '../components/404Page';
 
 const Router = () => {
   return (
@@ -9,6 +10,7 @@ const Router = () => {
       <Route path="/" element={<Home />} />
       <Route path="/portfolio" element={<Portolio />} />
       <Route path="/contact" element={<Contact />} />
+      <Route path="*" element={< Page />} />
     </Routes>
   )
 }
