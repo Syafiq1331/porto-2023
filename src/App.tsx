ee72339a21cb294a709977f2091dcd0cfeed728e
Syafiq Rizky Fauzi <bussinesyafiq@gmail.com>
2023-04-18 08:09:17 +0700

    fix:Change props in navbar to be static

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import Footer from "./components/Footer"
 import Navbar from "./components/Navbar"
 import Home from "./pages/Home"
 
 function App() {
   return (
-    <Navbar>
-    </Navbar>
+    <>
+      <Navbar />
+    </>
   )
 }
 
