1f963cb9d2e612c8113d37aadb9533fce26213d1
Syafiq Rizky Fauzi <bussinesyafiq@gmail.com>
2023-04-28 10:45:28 +0700

    feat:add github account

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -9,19 +9,23 @@ const Contact = () => {
           <div>
             <span className='flex items-center gap-4'>
               <i className='bx bxs-envelope text-red-500 text-4xl' ></i>
-              <a href='mailto:syafiqbussines@gmail.com' className='text-slate-800/70 dark:text-slate-50 underline text-md md:text-lg'>syafiqbussines@gmail.com</a>
+              <a href='mailto:syafiqbussines@gmail.com' className='text-slate-50 underline text-md md:text-lg'>syafiqbussines@gmail.com</a>
             </span>
             <span className='flex items-center gap-4'>
               <i className='bx bxl-telegram text-blue-500 text-4xl' ></i>
-              <a href='http://t.me/syafiq1331' className='text-slate-800/70 dark:text-slate-50 underline text-md md:text-lg'>Syafiqrzf</a>
+              <a href='http://t.me/syafiq1331' className='text-slate-50 underline text-md md:text-lg'>Syafiqrzf</a>
             </span>
             <span className='flex items-center gap-4'>
               <i className='bx bxl-linkedin-square text-blue-800 text-4xl' ></i>
-              <a href='https://www.linkedin.com/in/syafiqrzf1331/' target='_blank' className='text-slate-800/70 dark:text-slate-50 underline text-md md:text-lg'>Rizky Fauzi</a>
+              <a href='https://www.linkedin.com/in/syafiqrzf1331/' target='_blank' className='text-slate-50 underline text-md md:text-lg'>Rizky Fauzi</a>
             </span>
             <span className='flex items-center gap-4'>
               <i className='bx bxl-instagram-alt text-[#E1306C] text-4xl' ></i>
-              <a href='https://instagram.com/syafiqrzf' target='_blank' className='text-slate-800/70 dark:text-slate-50 underline text-md md:text-lg'>Syafiqrzf</a>
+              <a href='https://instagram.com/syafiqrzf' target='_blank' className='text-slate-50 underline text-md md:text-lg'>Syafiqrzf</a>
+            </span>
+            <span className='flex items-center gap-4'>
+              <i className='bx bxl-github text-[black] text-4xl'></i>
+              <a href='https://github.com/Syafiq1331' target='_blank' className='text-slate-50 underline text-md md:text-lg'>Syafiq Rizky</a>
             </span>
           </div>
         </div>
